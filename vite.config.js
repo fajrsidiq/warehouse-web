b400d8eeb265e51ce10e2b7b886bfc72bca009ce
fajrsidiq <fajarsidiqm55@gmail.com>
2023-09-07 18:32:31 +0700

    update 1.3

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  server: {
+    host: '0.0.0.0',
+    port: 3000, 
+  },
+  plugins: [
+    laravel({
+      input: [
+        'resources/sass/app.scss',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
 });
